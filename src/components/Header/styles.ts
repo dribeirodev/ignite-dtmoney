4c2c97f5600112dfb6be990d985988ff319fb128
diego.ribeiro <diego.ribeiro@gazin.com.br>
2023-04-02 20:56:53 -0300

    feat: implement component header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background-color: var(--blue-light);
+`;
+
+export const Content = styled.div`
+  margin: 0 auto;
+  max-width: 1120px;
+
+  padding: 2rem 1rem 12rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  button {
+    font-size: 1rem;
+    color: #ffffff;
+    background: var(--gray-light);
+    border: 0;
+    padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+
+    transition: filter 0.2s ease-in-out;
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
