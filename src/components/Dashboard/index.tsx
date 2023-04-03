7f364c21b05a18c041733bdeb0c36a43b8774f43
diego.ribeiro <diegoribeirodeveloper@gmail.com>
2023-04-02 21:40:29 -0300

    feat: implement component transactions table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,9 +1,11 @@
 import { Summary } from '../Summary';
 
+import { TransactionsTable } from '../TransactionsTable';
 import { Container } from './styles';
 
 export const Dashboard = () => (
   <Container>
     <Summary />
+    <TransactionsTable />
   </Container>
 );
